ffddca9fda034398407e3ccb684c839f6c93beb3
Shivang Yadav <125182653+shivang-16@users.noreply.github.com>
2025-12-20 15:58:56 +0530

    Comment out user selector and bid messages
    Commented out user selector and bid message for clarity.

--- src/app/(root)/project/[projectId]/_components/BidList.tsx
+++ src/app/(root)/project/[projectId]/_components/BidList.tsx
@@ -5,14 +5,14 @@ import React from "react";
 import { FaCalendarAlt, FaUserCircle } from "react-icons/fa";
 
 const BidList = ({ project }: { project: ProjectDataProps }) => {
-  const user = useAppSelector(state => state.user.user);
+  // const user = useAppSelector(state => state.user.user);
 
   return (
     <>
       {project.bids.length === 0 ? (
-        <h2 className="text-base md:text-[20px] font-medium text-gray-400">
-          No bids have been placed yet.
-        </h2>
+        // <h2 className="text-base md:text-[20px] font-medium text-gray-400">
+        //   No bids have been placed yet.
+        // </h2>
       ) : (
         <h2 className="text-xl md:text-[23px] pb-3 font-semibold text-gray-700 border-b-2 border-gray-200">
           <span className="text-teal-600">{project.bids.length} bids</span> have been placed.
@@ -43,9 +43,9 @@ const BidList = ({ project }: { project: ProjectDataProps }) => {
                   <p className="text-sm text-gray-700 flex items-center gap-2 mt-2">
                     <FaCalendarAlt className="text-teal-500" />
                     Deadline:
-                    <span className="font-medium text-teal-700">
+                    {/* <span className="font-medium text-teal-700">
                       {bid.deliveredIn.days} days
-                    </span>
+                    </span> */}
                   </p>
                 </div>
 
